0366be5037a01d77134b1d030dfb3c788097838d
simonhawkinsarup <simon.hawkins@arup.com>
2021-02-13 16:08:55 +0000

    tidying

--- app.js
+++ app.js
@@ -15,26 +15,16 @@ function cameraStart() {
         .getUserMedia(constraints)
         .then(function(stream) {
             track = stream.getTracks()[0];
-            
             cameraView.srcObject = stream;
-           /* canvasContext = cameraView.getContext('2d');
-            canvasContext.translate(w, 0);
-            canvasContext.scale(-1, 1); */
-            
         })
         .catch(function(error) {
             console.error("Oops. Something is broken.", error);
         });
 }
 
-// Take a picture when cameraTrigger is tapped
+// Show the frog
 cameraTrigger.onclick = function() {
-    /*cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");*/
     cameraOutput.classList.add("taken");
-    // track.stop();
 };
 
 // Start the video stream when the window loads
